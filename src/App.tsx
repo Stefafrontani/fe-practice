7e15f8811e5376195419855533cba504eb2d2844
Stefano Frontani <stefano.frontani@globant.com>
2020-05-05 16:41:12 -0300

    initial boiler plate

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
+const dependenciesInstalled : string[] = ['redux', 'react-redux', 'redux-thunk', 'typescript'];
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let titleApp : string = 'Traffic Practice';
+    return (
+        <div className="App">
+            <h1>{titleApp}</h1>
+            <ul>
+                {
+                    dependenciesInstalled.map((dependency) => {
+                        return <li>{dependency}</li>
+                    })
+                }
+            </ul>
+        </div>
+    );
 }
 
 export default App;

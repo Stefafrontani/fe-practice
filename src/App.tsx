804928f1cb2cdfd2172413f8c4fc027514cca745
Stefano Frontani <stefano.frontani@globant.com>
2020-05-14 15:26:34 -0300

    Remove whitespace

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Nav from './components/Nav';  
+import Nav from './components/Nav';
 
 // Containers
 import Home from './containers/Home/';

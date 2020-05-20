841d30c6e2d2d7bfd313438ca365dc80469cfb5e
Stefano Frontani <stefano.frontani@globant.com>
2020-05-20 16:31:19 -0300

    Add firebase config from the client side - React

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,21 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { BrowserRouter as Router } from 'react-router-dom';
 
+import * as firebase from 'firebase/app';
+import 'firebase/analytics';
+
+firebase.initializeApp({
+  "apiKey": "AIzaSyDaI8NDI_uU5hpaeXXyMOtPBrf7hP2qjvY",
+  "appId": "1:56870407467:web:5f4bc1221fec08e5ac496e",
+  "authDomain": "e-law-83343.firebaseapp.com",
+  "databaseURL": "https://e-law-83343.firebaseio.com",
+  "measurementId": "G-H7ZSE1V3W6",
+  "messagingSenderId": "56870407467",
+  "projectId": "e-law-83343",
+  "storageBucket": "e-law-83343.appspot.com"
+});
+firebase.analytics();
+
 ReactDOM.hydrate(
   <Router>
     <App />

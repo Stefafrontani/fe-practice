3a2795ce18d06c20447cb14b42de2fb6331229bb
Stefano Frontani <stefano.frontani@globant.com>
2020-05-20 11:58:03 -0300

    Fix react router to make every route accesible by url+enter

--- server/server.js
+++ server/server.js
@@ -12,26 +12,30 @@ const PORT = 8000;
 
 const app = express();
 
-app.use('^/$', (req, res, next) => {
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
+
+app.use('/*', (req, res, next) => {
   fs.readFile(path.resolve('./build/index.html'), 'utf-8', (err, data) => {
     if (err) {
       console.log(err);
       return res.status(500).send("Error");
     }
+
+    const context = {};
+    const markup = ReactDOMServer.renderToString(
+      <StaticRouter location={req.url} context={context}>
+        <App />
+      </StaticRouter>
+    )
+
     return res.send(
       data.replace(
-        '<div id="root"></div>', 
-        `<div id="root">${ReactDOMServer.renderToString(
-          <StaticRouter location={req.url} context={{}}>
-            <App />
-          </StaticRouter>
-        )}</div>`
+        '<div id="root"></div>',
+        `<div id="root">${markup}</div>`
       )
     );
   })
 });
-
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.listen(PORT, () => {
   console.log(`App launched on ${PORT}`);

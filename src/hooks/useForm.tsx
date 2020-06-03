f4c063321c9c83181a20bf599ae458fd632657ea
Carlos Alberto Jimenez Alcalde <carlos.jimenez@globant.com>
2020-06-03 14:27:55 -0500

    useForm hook is added

---
+++ src/hooks/useForm.tsx
@@ -0,0 +1,124 @@
+import React, { useState, useMemo } from 'react';
+
+// Interfaces START
+
+interface Validator {
+  (value: string): string;
+}
+
+interface FormInput {
+  field: string;
+  value: string;
+  validators: Validator[];
+}
+interface ParameterFunction<T> {
+  (value: FormInput): T;
+}
+
+interface Dictionary<T> {
+  [key: string]: T;
+}
+
+// Interfaces END
+
+// Hook START
+const useForm = (settings: FormInput[]) => {
+  const createObjectFrom = (
+    arraySource: FormInput[],
+    keyFunction: ParameterFunction<string>,
+    valueFunction: ParameterFunction<any>
+  ) =>
+    arraySource.reduce((object: Dictionary<any>, item: FormInput) => {
+      object[keyFunction(item)] = valueFunction(item);
+      return object;
+    }, {});
+
+  const calculateErrorList = (
+    value: string,
+    errorFunctions: Validator[]
+  ): string[] => {
+    let errorArray: string[] = [];
+    for (let errorFunction of errorFunctions) {
+      let errorMessage: string = errorFunction(value);
+      if (errorMessage) {
+        errorArray.push(errorMessage);
+      }
+    }
+    return errorArray;
+  };
+
+  const errorFunctionsByKey: Dictionary<[]> = useMemo(() => {
+    return createObjectFrom(
+      settings,
+      (item) => item.field,
+      (item) => item.validators || []
+    );
+  }, []);
+
+  const [fields, setFields] = useState<Dictionary<string>>(
+    createObjectFrom(
+      settings,
+      (item) => item.field,
+      (item) => item.value || ''
+    )
+  );
+  const [errors, setErrors] = useState<Dictionary<[]>>(
+    createObjectFrom(
+      settings,
+      (item) => item.field,
+      (item) => calculateErrorList(item.value, errorFunctionsByKey[item.field])
+    )
+  );
+
+  const disableForm: boolean = useMemo(() => {
+    return !![].concat(...Object.values(errors)).length;
+  }, [errors]);
+
+  const onChange = (event: React.ChangeEvent<{ name: any; value: any }>) => {
+    const { name, value } = event.target;
+
+    if (errorFunctionsByKey[name].length) {
+      const newErrorList: string[] = calculateErrorList(
+        value,
+        errorFunctionsByKey[name]
+      );
+      const oldErrorList: string[] = errors[name];
+
+      let shouldUpdateErrors: boolean =
+        newErrorList.length !== oldErrorList.length;
+
+      if (!shouldUpdateErrors) {
+        for (let i = 0; i < newErrorList.length; i++) {
+          if (newErrorList[i] !== oldErrorList[i]) {
+            shouldUpdateErrors = true;
+            break;
+          }
+        }
+      }
+
+      if (shouldUpdateErrors) {
+        setErrors((previousErrors) => ({
+          ...previousErrors,
+          [name]: newErrorList,
+        }));
+      }
+    }
+
+    setFields((previousFields) => ({ ...previousFields, [name]: value }));
+  };
+
+  return { fields, onChange, errors, disableForm };
+};
+// Hook END
+
+// Validator functions START
+export const isRequired: Validator = (value: string) =>
+  !value ? 'is required' : '';
+
+export const isEmail: Validator = (value) =>
+  !/.+@.+\.[A-Za-z]+$/.test(value) ? 'has wrong format' : '';
+
+export const maxChars = (maxChars: number): Validator => (value: string) =>
+  value.length >= maxChars ? `size is greater than ${maxChars} characters` : '';
+// Validator functions END
+export default useForm;
